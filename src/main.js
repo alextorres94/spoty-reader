d1750a032f5052f17463ac3017c4f8c0388336d5
alextorres94 <torres.erades.alex@gmail.com>
2019-10-06 13:36:58 +0200

    Testing vue

--- src/main.js
+++ src/main.js
@@ -1,7 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+const NotFound = { template: '<p>Page not found</p>' }
+const Home = { template: App }
+const About = { template: '<p>about page</p>' }
+
+const routes = {
+  '/': Home,
+  '/about': About
+}
+
+
 new Vue({
   el: '#app',
-  render: h => h(App)
+  data: {
+    currentRoute: window.location.pathname
+  },
+  computed: {
+    ViewComponent () {
+      return routes[this.currentRoute] || NotFound
+    }
+  },
+  render (h) { return h(this.ViewComponent) }
 })
